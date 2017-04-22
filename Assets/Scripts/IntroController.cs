63b1f0552a7f4eec70b3768a56541aeec3b3cd70
Izzo <hristoz2004@gmail.com>
2017-04-22 19:05:56 +0100

    Add main menu and intro

---
+++ Assets/Scripts/IntroController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroController : MonoBehaviour {
+
+    [SerializeField]
+    private string _nextScene;
+
+    // Update is called once per frame
+    void Update() {
+        if( Input.anyKey || Input.GetMouseButtonDown( 0 ) ) {
+            LoadNextScene();
+        }
+    }
+
+    private void LoadNextScene() {        
+        SceneManager.LoadScene( _nextScene );
+    }
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour {
+
+    public void LoadScene( string name ) {
+        SceneManager.LoadScene( name );
+    }
+}
