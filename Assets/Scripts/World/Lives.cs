7a1192738cbc5a09012e695cedcc848f2b33f094
chvck <chvckd@gmail.com>
2017-04-22 12:30:39 +0100

    Add lives script

---
+++ Assets/Scripts/World/Lives.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Lives : MonoBehaviour {
+    [SerializeField]
+    private int numberStartLives = 5;
+
+    private int numberLivesRemaining;
+    private UnityEvent onLivesEmptyEvent = new UnityEvent();
+
+    private void Awake() {
+        numberLivesRemaining = numberStartLives;
+    }
+
+    private void RemoveLife() {
+        numberLivesRemaining = numberLivesRemaining - 1;
+    }
+
+    private bool OutOfLives() {
+        return numberLivesRemaining >= 0;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        RemoveLife();
+
+        if (OutOfLives()) {
+            onLivesEmptyEvent.Invoke();
+        }
+    }
+
+    public void AddListener(UnityAction listener) {
+        onLivesEmptyEvent.AddListener(listener);
+    }
+}
