cdb402e7fe1b6a29dae2961abecd6eefabf4fdf4
Izzo <hristoz2004@gmail.com>
2017-04-22 12:38:54 +0100

    Add WIP scripts

---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+
+    [SerializeField]
+    private Planet _planet;
+
+    [SerializeField]
+    private float _orbitAngle;
+
+    [SerializeField]
+    private float _orbitDistance;
+
+    public enum MoveDirection
+    {
+        Left = -1,
+        None = 0,        
+        Right = 1,
+    }
+
+    public float Speed
+    {
+        get { return _speed; }
+        set { _speed = value; }
+    }
+
+    public MoveDirection Direction
+    {
+        get; set;
+    }
+
+    public Planet Planet
+    {
+        get { return _planet; }
+        set { _planet = value; }
+    }
+
+    public float OrbitAngle
+    {
+        get { return _orbitAngle; }
+        private set { _orbitAngle = value; }
+    }
+
+    public float OrbitDistance
+    {
+        get { return _orbitDistance; }
+        set { _orbitDistance = value; }
+    }
+
+    private void FixedUpdate()
+    {
+        int direction = (int) Direction;
+        float anglePerSecond = Speed / Planet.Permieter * direction;
+        float deltaAngle = Time.fixedDeltaTime * anglePerSecond;
+
+        OrbitAngle += deltaAngle;
+
+        Vector3 targetPosition, targetFacing;
+        Planet.SampleOrbit2D( OrbitAngle, OrbitDistance, 
+                              out targetPosition, out targetFacing );
+
+        transform.forward = targetFacing;
+        transform.position = targetPosition;
+    }
+}
---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    [SerializeField]
+    private float _radius;
+
+    public float Radius
+    {
+        get { return _radius; }
+        set { _radius = value; }
+    }
+
+    public float Permieter
+    {
+        get { return 2 * Mathf.PI * Radius; }
+    }
+
+    public void SampleOrbit2D( float angle, 
+                               float distance, 
+                               out Vector3 position, 
+                               out Vector3 normal )
+    {
+        // Polar to cartesian coordinates
+        float x = Mathf.Cos( angle ) * distance;
+        float y = Mathf.Sin( angle ) * distance;
+
+        Vector3 dispalcement = new Vector3( x, 0, y );
+        Vector3 center = transform.position;
+        position = center + dispalcement;
+        normal = dispalcement.normalized;
+    }    
+}
