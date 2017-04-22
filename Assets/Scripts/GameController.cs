5a92679dc70214eb3a972b144fb2cdab29b1547b
Izzo <hristoz2004@gmail.com>
2017-04-22 14:32:59 +0100

    Make the ball move

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    [SerializeField]
+    private Rigidbody ball;
+
+    private void Start() {
+
+        float randomAngle = Random.value;
+        Vector3 randomDirection = new Vector3(
+            Mathf.Cos( randomAngle ),
+            0,
+            Mathf.Sin( randomAngle ) );
+
+        ball.AddForce( randomDirection * 10, ForceMode.Impulse );
+    }
+
+}
