244940f88e8ace1a9db05bcd70de013c6824703d
Izzo <hristoz2004@gmail.com>
2017-04-22 17:45:32 +0100

    Limit movement to an arc and make editor update based on parameters.

--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -1,5 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
+[ExecuteInEditMode]
 public class PaddleController : MonoBehaviour {
     [SerializeField]
     private float _speed;
@@ -8,10 +10,13 @@ public class PaddleController : MonoBehaviour {
     private Planet _planet;
 
     [SerializeField]
+    private float _orbitDistance;
+
+    [SerializeField]
     private float _orbitAngle;
 
     [SerializeField]
-    private float _orbitDistance;
+    private Vector2 _orbitArc;
 
     [SerializeField, Range( -1, 1 )]
     private float _direction = 0;
@@ -33,7 +38,21 @@ public class PaddleController : MonoBehaviour {
 
     public float OrbitAngle {
         get { return _orbitAngle; }
-        private set { _orbitAngle = value; }
+        private set {
+            _orbitAngle = Mathf.Repeat( value, 360 );
+            _orbitAngle = ClampAngle( _orbitAngle, _orbitArc.x, _orbitArc.y );
+        }
+    }
+
+    private static float ClampAngle( float angle, float min, float max ) {
+        if( angle < 90 || angle > 270 ) {       // if angle in the critic region...
+            if( angle > 180 ) angle -= 360;  // convert all angles to -180..+180
+            if( max > 180 ) max -= 360;
+            if( min > 180 ) min -= 360;
+        }
+        angle = Mathf.Clamp( angle, min, max );
+        if( angle < 0 ) angle += 360;  // if angle negative, convert to 0..360
+        return angle;
     }
 
     public float OrbitDistance {
@@ -46,7 +65,25 @@ public class PaddleController : MonoBehaviour {
         float anglePerSecond = Speed / Planet.Permieter * Direction * Mathf.Rad2Deg;
         float deltaAngle = Time.fixedDeltaTime * anglePerSecond;
 
-        OrbitAngle += deltaAngle;        
+        OrbitAngle += deltaAngle;
+
+        UpdatePositionAndRotation();
+    }
+
+#if UNITY_EDITOR
+
+    private void Update() {
+        if( !Application.isPlaying ) {
+            UpdatePositionAndRotation();
+        }
+    }
+
+#endif
+
+    private void UpdatePositionAndRotation() {
+        if( !Planet ) {
+            return;
+        }
 
         Vector3 targetPosition, targetFacing;
         Planet.SampleOrbit2D( OrbitAngle * Mathf.Deg2Rad, OrbitDistance,
