4f925224be6e0c378ba37422e8b8aa8ec894c68f
chvck <chvckd@gmail.com>
2017-04-22 23:19:04 +0100

    Add some phone controls

--- Assets/Scripts/Core/PlayerController.cs
+++ Assets/Scripts/Core/PlayerController.cs
@@ -4,9 +4,26 @@ public class PlayerController : MonoBehaviour {
 
     [SerializeField]
     private PaddleController _paddleController;
+
+    private float halfScreenHeight;
+
+    private void Start() {
+        halfScreenHeight = Screen.height / 2;
+    }
     
     private void Update() {
-        float direction = Input.GetAxis( "Vertical" );
+        float direction = 0.0f;
+        if (Input.touchCount > 0) {
+            Touch touch = Input.touches[0];
+            float posY = touch.position.y;
+            if ( posY > halfScreenHeight ) {
+                direction = 1.0f;
+            } else if ( posY < halfScreenHeight ) {
+                direction = -1.0f;
+            }
+        } else {
+            direction = Input.GetAxis( "Vertical" );
+        }
         _paddleController.Direction = direction;
     }
 }
