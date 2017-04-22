76824d197c3af4350d7bbd5b219635dbcf069cd9
Izzo <hristoz2004@gmail.com>
2017-04-22 13:46:42 +0100

    Add player controll script

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private PaddleController _paddleController;
+    
+    private void Update() {
+        float direction = Input.GetAxis( "Vertical" );
+        _paddleController.Direction = direction;
+    }
+}
