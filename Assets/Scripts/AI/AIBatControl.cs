2e2b046b99959a31cd46ca646d3b96f44ddf7eb1
chvck <chvckd@gmail.com>
2017-04-22 14:11:30 +0100

    Add rudimentary AI control

---
+++ Assets/Scripts/AI/AIBatControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIBatControl : MonoBehaviour {
+    [SerializeField]
+    private Ball ball;
+    [SerializeField]
+    private PaddleController paddleController;
+
+	private void FixedUpdate() {
+        Vector3 paddlePosition = paddleController.transform.position;
+        Vector3 ballPosition = ball.transform.position;
+
+        float direction = 0.0f;
+        float ballZ = ballPosition.z;
+        float paddleZ = paddlePosition.z;
+        float positionDifference = ballZ - paddleZ;
+        Debug.Log(positionDifference);
+        if( positionDifference > 0 ) {
+            direction = -1.0f;
+        } else if( positionDifference < 0 ) {
+            direction = 1.0f;
+        }
+        paddleController.Direction = direction;
+	}
+}
