4f38fdd2a26829db68e5828e19960519000fed59
chvck <chvckd@gmail.com>
2017-04-22 15:51:07 +0100

    Reset ball position on world hit

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
 
+    private 
+
 	// Use this for initialization
 	void Start () {
 		
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,9 +6,26 @@ public class GameController : MonoBehaviour {
 
     [SerializeField]
     private Rigidbody ball;
+    [SerializeField]
+    private Planet[] worlds;
+
+    private Vector3 ballStartPosition;
 
     private void Start() {
+        foreach (var world in worlds)
+        {
+            world.AddHitListener(OnWorldHit);
+        }
+        ballStartPosition = ball.transform.position;
 
+        KickOff();
+    }
+
+    private void OnWorldHit() {
+        Reset();
+    }
+
+    private void KickOff() {
         float randomAngle = Random.value;
         Vector3 randomDirection = new Vector3(
             Mathf.Cos( randomAngle ),
@@ -16,6 +33,12 @@ public class GameController : MonoBehaviour {
             Mathf.Sin( randomAngle ) );
 
         ball.AddForce( randomDirection * 10, ForceMode.Impulse );
+    }
+
+    public void Reset() {
+        ball.transform.position = ballStartPosition;
+        ball.velocity = Vector3.zero;
+        KickOff();
     }
 
 }
