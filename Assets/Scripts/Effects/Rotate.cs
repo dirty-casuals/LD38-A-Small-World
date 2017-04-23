6cc37ca48b9eae6c447e4060745d60c78b2c2bbb
Izzo <hristoz2004@gmail.com>
2017-04-23 20:47:27 +0100

    Add rotation to planets, remove HUD lives, move scripts

---
+++ Assets/Scripts/Effects/Rotate.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    [SerializeField]
+    Vector3 _rotation;
+
+    void Update() {
+        transform.Rotate( _rotation * Time.deltaTime );
+    }
+}
